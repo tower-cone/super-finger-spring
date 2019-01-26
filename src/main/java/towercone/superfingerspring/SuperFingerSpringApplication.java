56775a9d6a95cb6c738affd88984a56f600893fd
Pavel <p97p@ya.ru>
2019-01-26 15:50:53 +0300

    init (using https://start.spring.io/)

---
+++ src/main/java/towercone/superfingerspring/SuperFingerSpringApplication.java
@@ -0,0 +1,14 @@
+package towercone.superfingerspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperFingerSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperFingerSpringApplication.class, args);
+	}
+
+}
+
